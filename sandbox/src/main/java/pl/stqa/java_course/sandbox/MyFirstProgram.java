7f21fe27b5f43d21508749f2b4b365865d1fd971
alla-d <aviator.nr1@gmail.com>
2018-11-18 21:43:09 +0100

    Change editoring settings

--- sandbox/src/main/java/pl/stqa/java_course/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/stqa/java_course/sandbox/MyFirstProgram.java
@@ -2,10 +2,8 @@ package pl.stqa.java_course.sandbox;
 
 public class MyFirstProgram {
 
-public static void main(String[] args) {
-
-System.out.println("Hello, world!");
-
-}
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 
 }
