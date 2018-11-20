f31b408602580ea5cd1b9e15597c8d581e835f70
alla-d <aviator.nr1@gmail.com>
2018-11-20 19:31:28 +0100

    Add class Distance and change class Point

---
+++ sandbox/src/main/java/pl/stqa/java_course/sandbox/Distance.java
@@ -0,0 +1,11 @@
+package pl.stqa.java_course.sandbox;
+
+public class Distance {
+
+  public static void main(String[] args) {
+    Point p1 = new Point(5.5, 7.9);
+    Point p2 = new Point(3.2, 5.3);
+    System.out.println("The distance between Point p1 to Point p2 (using static function) = " + Point.distance(p1, p2));
+    System.out.println("The distance between Point p1 to Point p2 (using method) = " + p1.distance2(p2));
+  }
+}
--- sandbox/src/main/java/pl/stqa/java_course/sandbox/Point.java
+++ sandbox/src/main/java/pl/stqa/java_course/sandbox/Point.java
@@ -15,11 +15,10 @@ public class Point {
     return dist;
   }
 
-  public static void main(String[] args) {
-    Point p1 = new Point(3.5, 5.9);
-    Point p2 = new Point(3.2, 5.3);
-    System.out.println("The distance between Point p1 to Point p2 = " + Point.distance(p1, p2));
+
+  public double distance2(Point p2) {
+    double dist = Math.sqrt(Math.pow(this.x - p2.x, 2.0) + Math.pow(this.y - p2.y, 2.0));
+    return dist;
   }
 
 }
-
