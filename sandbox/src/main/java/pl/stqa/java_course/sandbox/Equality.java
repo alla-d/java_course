6dd5754cd3a43f03282a420cf1c3e1cf282e09ac
alla-d <aviator.nr1@gmail.com>
2018-11-28 22:26:50 +0100

    Added class Equality

---
+++ sandbox/src/main/java/pl/stqa/java_course/sandbox/Equality.java
@@ -0,0 +1,14 @@
+package pl.stqa.java_course.sandbox;
+
+public class Equality {
+
+  public static void main (String [] args){
+    String s1 = "firefox";
+    //String s2 = new String(s1);
+    // String s2 =s1;
+    String s2 = "fire" + "fox";
+
+    System.out.println(s1 == s2);
+    System.out.println(s1.equals(s2));
+  }
+}
