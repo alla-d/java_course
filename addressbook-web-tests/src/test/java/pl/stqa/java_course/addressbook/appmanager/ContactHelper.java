a59f791c3aed933e968dcb3dded666a843fbac17
alla-d <aviator.nr1@gmail.com>
2018-12-01 23:02:14 +0100

    class NavigationHelper has been changed

--- addressbook-web-tests/src/test/java/pl/stqa/java_course/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/pl/stqa/java_course/addressbook/appmanager/ContactHelper.java
@@ -2,7 +2,6 @@ package pl.stqa.java_course.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import pl.stqa.java_course.addressbook.model.GroupDataContact;
--- addressbook-web-tests/src/test/java/pl/stqa/java_course/addressbook/tests/CreateNewContact.java
+++ addressbook-web-tests/src/test/java/pl/stqa/java_course/addressbook/tests/CreateNewContact.java
@@ -10,6 +10,7 @@ public class CreateNewContact extends TestBase{
     app.getContactHelper().gotoNewContactPage();
     app.getContactHelper().fillContactForm(new GroupDataContact("Name", "Middle name", "Surname", "test1", "Mr", "Test", "test, test street, 01000", "0123456", "1234567", "01234568", "0123", "test@test.com", "test, test, 01000", "test1"), true);
     app.getContactHelper().submitContactCreation();
+    app.getNavigationHelper().gotoHomePage();
   }
 
 }
