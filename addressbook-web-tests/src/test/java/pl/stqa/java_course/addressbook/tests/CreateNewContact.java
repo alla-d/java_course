971d9e8e7dc021a5f219d960ec21b18998fcb810
alla-d <aviator.nr1@gmail.com>
2018-12-02 21:28:47 +0100

    checking of the preconditions has been realized

--- addressbook-web-tests/src/test/java/pl/stqa/java_course/addressbook/tests/CreateNewContact.java
+++ addressbook-web-tests/src/test/java/pl/stqa/java_course/addressbook/tests/CreateNewContact.java
@@ -8,7 +8,7 @@ public class CreateNewContact extends TestBase{
   @Test
   public void testCreateNewContact() throws Exception {
     app.getContactHelper().gotoNewContactPage();
-    app.getContactHelper().fillContactForm(new GroupDataContact("Name", "Middle name", "Surname", "test1", "Mr", "Test", "test, test street, 01000", "0123456", "1234567", "01234568", "0123", "test@test.com", "test, test, 01000", "test1"), true);
+    app.getContactHelper().fillContactForm(new GroupDataContact("Name", "Middle name", "Surname", "test1", "Mr", null, "test, test street, 01000", "0123456", "1234567", "01234568", "0123", "test@test.com", "test, test, 01000", "test1"), true);
     app.getContactHelper().submitContactCreation();
     app.getNavigationHelper().gotoHomePage();
   }
--- addressbook-web-tests/src/test/java/pl/stqa/java_course/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/java_course/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package pl.stqa.java_course.addressbook.tests;
 
 import org.testng.annotations.Test;
+import pl.stqa.java_course.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase{
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", "test2", null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
