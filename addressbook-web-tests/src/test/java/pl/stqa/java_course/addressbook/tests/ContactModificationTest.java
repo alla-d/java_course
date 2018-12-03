9cbb4e0b53d2e296398a3910d9925ee1a01d0996
alla-d <aviator.nr1@gmail.com>
2018-12-04 00:02:21 +0100

    added class Primes and PrimeTests

--- addressbook-web-tests/src/test/java/pl/stqa/java_course/addressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/pl/stqa/java_course/addressbook/tests/ContactModificationTest.java
@@ -8,7 +8,7 @@ public class ContactModificationTest extends TestBase {
   @Test
   public void testContactModification(){
     if (! app.getContactHelper().isThereAContact()){
-      app.getContactHelper().createContact(new GroupDataContact("Name", "Middle name", "Surname", "test1", "Mr", null, "test, test street, 01000", "0123456", "1234567", "01234568", "0123", "test@test.com", "test, test, 01000", "test1"), true);
+      app.getContactHelper().createContact(new GroupDataContact("Name", "Middle name", "Surname", "test1", "Mr", null, "test, test street, 01000", "0123456", "1234567", "01234568", "0123", "test@test.com", "test, test, 01000", "[none]"), true);
     }
     app.getContactHelper().selectContact();
     app.getContactHelper().fillContactForm(new GroupDataContact("update", "Middle name2", "Surname update", "test update", "Mrs", "Test update", "test, update street, 01000", "(+11)0123456", "1234567", "01234568", "0123", "test@test.com", "test, test, 01000", null), false);
---
+++ sandbox/src/test/java/pl/stqa/java_course/sandbox/PrimeTests.java
@@ -0,0 +1,28 @@
+package pl.stqa.java_course.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeTests {
+
+  @Test
+  public void testPrime() {
+    Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+  }
+
+  @Test
+  public void testNonPrime() {
+    Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+  }
+
+  @Test (enabled = false)
+  public void testPrimeLong() {
+    long n = Integer.MAX_VALUE;
+        Assert.assertTrue(Primes.isPrime(n));
+  }
+
+  @Test
+  public void testPrimeFast() {
+    Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+  }
+}
