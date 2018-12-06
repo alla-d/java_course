422b3992771a529b9290553ea18e3ee6bb9b5911
alla-d <aviator.nr1@gmail.com>
2018-12-06 21:27:03 +0100

    GroupCreationTests has been changed

--- addressbook-web-tests/src/test/java/pl/stqa/java_course/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/pl/stqa/java_course/addressbook/model/GroupData.java
@@ -3,34 +3,32 @@ package pl.stqa.java_course.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-  private final String id;
+
+
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
-  public String getId() {
+  public int getId() {
     return id;
   }
 
-  public GroupData(String id, String name, String header, String footer) {
+  public void setId(int id) {
     this.id = id;
+  }
+  public GroupData(String name, String header, String footer) {
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return Objects.equals(id, groupData.id) &&
-            Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name);
+  public GroupData(int id, String name, String header, String footer) {
+    this.id = id;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
   }
 
   @Override
@@ -51,5 +49,20 @@ public class GroupData {
 
   public String getFooter() {
     return footer;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    if (id != groupData.id) return  false;
+     return name != null ? name.equals(groupData.name) : groupData.name == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id;
+    return 31 * result+ (name != null ? name.hashCode() : 0);
   }
 }
