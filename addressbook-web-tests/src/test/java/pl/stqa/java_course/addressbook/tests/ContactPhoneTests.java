7b35b1555fdcb4c2280b4e87caefcc9bd285de83
alla-d <aviator.nr1@gmail.com>
2018-12-18 00:13:18 +0100

    create class ContactPhoneTests

---
+++ addressbook-web-tests/src/test/java/pl/stqa/java_course/addressbook/tests/ContactPhoneTests.java
@@ -0,0 +1,47 @@
+package pl.stqa.java_course.addressbook.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pl.stqa.java_course.addressbook.model.ContactData;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class ContactPhoneTests extends TestBase {
+
+  @BeforeMethod
+  public void ensurePreconditions(){
+    app.goTo().homePage();
+    if (app.contact().all().size() == 0){
+      app.contact().create(new ContactData()
+              .withContactName("Name").withLastName("Surname").withHomePhone("1111")
+              .withMobilePhone("01234568").withWorkPhone("2222").withGroup("[none]"), true);
+    }
+    app.goTo().homePage();
+  }
+
+  @Test
+  public void testContactPhones(){
+    app.goTo().homePage();
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+    assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
+
+  }
+
+  private String mergePhones(ContactData contact) {
+    return Arrays.asList(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone())
+            .stream().filter((s) -> !s.equals(""))
+            .map(ContactPhoneTests::cleaned)
+            .collect(Collectors.joining("\n"));
+  }
+
+  public static String cleaned(String phone) {
+    return phone.replaceAll("\\s", "").replaceAll("[-()]", "");
+
+  }
+}
