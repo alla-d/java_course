e18aaa2232427eee8fe64770660c5a019dd07447
alla-d <aviator.nr1@gmail.com>
2019-01-09 20:48:39 +0100

    Added new module for Mantis test

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,27 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+public class TestBase {
+
+
+  //protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  // CHROME
+  protected final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+  // IE
+  // protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
+
+  @BeforeSuite(alwaysRun = true)
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown()  {
+    app.stop();
+  }
+
+}
